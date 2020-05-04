506cefd2132751600cac2ab8a270d352af66ff70
lisy0123 <leesese3320@gmail.com>
2020-05-04 22:46:37 +0900

    update

--- C/03_InputOutput_function_and_Operator/ex01.c
+++ C/03_InputOutput_function_and_Operator/ex01.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main ()
 {
     int res;
 
     printf("4*8/16 = "); //2^(2+3-4)
-    res = 4<<3>>4;
+    res = 4 << 3 >> 4;
     printf("%d", res);
-    return(0);
+    return (0);
 }
