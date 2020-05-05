e52861335e97528fd036736c999fd2f620fb8a1f
lisy0123 <leesese3320@gmail.com>
2020-05-05 23:12:18 +0900

    Data structure update

--- Data_structure/stack_example.c
+++ Data_structure/stack_example.c
@@ -5,68 +5,68 @@ int stack[10];
 
 int POP(int top)
 {
-    if (top == -1)
-    {
-        printf ("Stack is Empty!\n");
-        return (0);
-    }
-    else
-        return (stack[top--]);
+	if (top == -1)
+	{
+		printf ("Stack is Empty!\n");
+		return (0);
+	}
+	else
+		return (stack[top--]);
 }
 
 void PUSH(int top, int item)
 {
-    if (top >= 10 - 1)
-    {
-        printf("Stack is Full!\n");
-        return;
-    }
-    else
-        stack[++top] = item;
+	if (top >= 10 - 1)
+	{
+		printf("Stack is Full!\n");
+		return;
+	}
+	else
+		stack[++top] = item;
 }
 
 char evalPostfix(char *exp)
 {
-    int op1, op2, value, i = 0;
-    int len = strlen(exp);
-    char symbol;
-    int top = -1;
+	int op1, op2, value, i = 0;
+	int len = strlen(exp);
+	char symbol;
+	int top = -1;
 
-    for (i = 0; i < len; i++)
-    {
-        symbol = exp[i];
-        if (symbol != '+' && symbol != '-' && symbol != '*' && symbol != '/')
-        {
-            value = symbol - '0';
-            PUSH(top, value);
-            top++;
-        }
-        else
-        {
-            op2 = POP(top);
-            top--;
-            op1 = POP(top);
-            top--;
-            switch (symbol)
-            {
-                case '+': PUSH(top, op1 + op2); top++; break;
-                case '-': PUSH(top, op1 - op2); top++; break;
-                case '*': PUSH(top, op1 * op2); top++; break;
-                case '/': PUSH(top, op1 / op2); top++; break;
-            }
-        }
-    }
-    return (POP(top));
+	for (i = 0; i < len; i++)
+	{
+		symbol = exp[i];
+		if (symbol != '+' && symbol != '-' && symbol != '*' && symbol != '/')
+		{
+			value = symbol - '0';
+			PUSH(top, value);
+			top++;
+		}
+		else
+		{
+			op2 = POP(top);
+			top--;
+			op1 = POP(top);
+			top--;
+			switch (symbol)
+			{
+				case '+': PUSH(top, op1 + op2); top++; break;
+				case '-': PUSH(top, op1 - op2); top++; break;
+				case '*': PUSH(top, op1 * op2); top++; break;
+				case '/': PUSH(top, op1 / op2); top++; break;
+			}
+		}
+	}
+	return (POP(top));
 }
 
 int main ()
 {
-    char s[10];
-    int res;
+	char s[10];
+	int res;
 
-    printf(">> ");
-    scanf("%s", s);
-    res = evalPostfix(s);
-    printf("result: %d",res);
-    return (0);
+	printf(">> ");
+	scanf("%s", s);
+	res = evalPostfix(s);
+	printf("result: %d",res);
+	return (0);
 }
